288fe58eae53f298908b8414bd9875ce13dd5fa0
alphasoe <alphasoe.as1998@gmail.com>
2020-11-11 11:32:46 +0630

    update toprated

--- app/src/main/java/com/myanmaritc/moviedb/ui/toprated/TopRatedFragment.kt
+++ app/src/main/java/com/myanmaritc/moviedb/ui/toprated/TopRatedFragment.kt
@@ -7,8 +7,13 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.GridLayoutManager
 import com.myanmaritc.moviedb.R
+import com.myanmaritc.moviedb.model.ResultsItem
+import com.myanmaritc.moviedb.ui.adapter.MovieAdapter
+import kotlinx.android.synthetic.main.fragment_top_rated.*
 
 class TopRatedFragment : Fragment() {
 
@@ -20,12 +25,29 @@ class TopRatedFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
         topRatedViewModel =
-                ViewModelProviders.of(this).get(TopRatedViewModel::class.java)
+               ViewModelProvider(this).get(TopRatedViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_top_rated, container, false)
 
-        topRatedViewModel.text.observe(viewLifecycleOwner, Observer {
-
-        })
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        var topRatedAdapter = MovieAdapter()
+
+        recycler_top_rated.apply {
+            recycler_top_rated.layoutManager=GridLayoutManager(context,2)
+            recycler_top_rated.adapter=topRatedAdapter
+        }
+
+        topRatedViewModel.gettopRated().observe(viewLifecycleOwner, Observer { topRated->
+            topRatedAdapter.addMovie(topRated.results as List<ResultsItem>)
+        })
+    }
+
+    override fun onResume() {
+        super.onResume()
+        topRatedViewModel.loadData()
     }
 }
--- app/src/main/java/com/myanmaritc/moviedb/ui/toprated/TopRatedViewModel.kt
+++ app/src/main/java/com/myanmaritc/moviedb/ui/toprated/TopRatedViewModel.kt
@@ -3,11 +3,32 @@ package com.myanmaritc.moviedb.ui.toprated
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.myanmaritc.moviedb.api.MovieClient
+import com.myanmaritc.moviedb.model.Movie
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class TopRatedViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is slideshow Fragment"
+    private var topRatedViewModel: MutableLiveData<Movie> = MutableLiveData()
+
+    fun gettopRated() = topRatedViewModel
+
+    fun loadData() {
+
+        var apiClient = MovieClient()
+        var apiCall = apiClient.getToprated()
+
+        apiCall.enqueue(object : Callback<Movie> {
+            override fun onFailure(call: Call<Movie>, t: Throwable) {
+
+            }
+
+            override fun onResponse(call: Call<Movie>, response: Response<Movie>) {
+                topRatedViewModel.value = response.body()
+            }
+
+        })
     }
-    val text: LiveData<String> = _text
 }
